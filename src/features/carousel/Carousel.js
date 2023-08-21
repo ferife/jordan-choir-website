30f353daf9994f5e52744891d608d32b4d318981
Fernando <ferife@icloud.com>
2023-08-21 18:30:32 -0500

    Initial setup for Carousel

---
+++ src/features/carousel/Carousel.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { UncontrolledCarousel } from "reactstrap";
+
+const picCarousel = ({slides}) => {
+    <UncontrolledCarousel />
+};
+
+export default picCarousel;
