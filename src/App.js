42c0ffead21c52ee9bc8ba39eb1d3a756f60c16c
Fernando <ferife@icloud.com>
2023-09-17 18:57:59 -0500

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import PicCarousel from './features/carousel/PicCarousel';
 import { Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -18,10 +17,9 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            {/* TODO: Go back to BrowserRouter, since I'm not using GitHub Pages */}
-            <HashRouter basename=''>
+            <BrowserRouter basename=''>
                 <App />
-            </HashRouter>
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 );
