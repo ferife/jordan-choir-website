6c6c517bde79236a7014292179323fbce78ccae1
Fernando <ferife@icloud.com>
2023-09-27 22:10:37 -0500

    Finally add school calendar

--- frontEnd/src/pages/CalendarPage.js
+++ frontEnd/src/pages/CalendarPage.js
@@ -1,7 +1,7 @@
 const CalendarPage = () => {
     return (
         <>
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FChicago&showTitle=1&showNav=1&showDate=1&showPrint=1&showTz=1&showTabs=1&title=Test%20Calendar%20Title&src=YjMzNDgzZTE2OWJmMzE1Yzg3YmIwNTdiY2JlMTYyZmYwYjJhMDhlMjU3MmExYzU5OTVhMTdlYzEwYWVlZWRhZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00" title="CalendarTest" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?src=jordanchoirbc%40gmail.com&ctz=America%2FChicago" title="JordanCalendar" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         </>
     );
 };
