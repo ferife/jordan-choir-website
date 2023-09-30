744e6509943677ef64924fc4a8f2efd047898881
Fernando <ferife@icloud.com>
2023-09-30 16:53:20 -0500

    Finally fix npm run build issue

--- frontEnd/functions/index.js
+++ frontEnd/functions/index.js
@@ -7,8 +7,22 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-function");
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1; // London is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${"BONG ".repeat(hours)}
+    </body>
+  </html>`);
+});
+
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
